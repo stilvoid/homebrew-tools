78307a81a5130e7735956f509420e15de6d0ee0d
Steve Engledow <steve@offend.me.uk>
2025-06-20 15:59:40 +0100

    Split the long line better

--- Formula/git-get.rb
+++ Formula/git-get.rb
@@ -11,7 +11,7 @@ class GitGet < Formula
   end
 
   test do
-    assert_equal "Usage: git get [--print-path] [--location <dir>] " +
-                 "<repository> [<args>]", shell_output("#{bin}/git-get -h").lines.first.strip
+    assert_equal "Usage: git get [--print-path] [--location <dir>] <repository> [<args>]", \
+                 shell_output("#{bin}/git-get -h").lines.first.strip
   end
 end
